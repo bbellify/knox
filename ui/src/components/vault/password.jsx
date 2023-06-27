ab0093872e1acc54a86743f80ff92d40a506948f
bbellify <brianpatrickbellinger@gmail.com>
2023-06-27 12:53:10 -0700

    fix copy pass bug - globbed

--- ui/src/components/vault/password.jsx
+++ ui/src/components/vault/password.jsx
@@ -3,14 +3,14 @@ import React from "react";
 export const password = () => {
   return (
     <>
-      <ion-icon name="ellipse"></ion-icon>
-      <ion-icon name="ellipse"></ion-icon>
-      <ion-icon name="ellipse"></ion-icon>
-      <ion-icon name="ellipse"></ion-icon>
-      <ion-icon name="ellipse"></ion-icon>
-      <ion-icon name="ellipse"></ion-icon>
-      <ion-icon name="ellipse"></ion-icon>
-      <ion-icon name="ellipse"></ion-icon>
+      <ion-icon name="ellipse" class="ellipse"></ion-icon>
+      <ion-icon name="ellipse" class="ellipse"></ion-icon>
+      <ion-icon name="ellipse" class="ellipse"></ion-icon>
+      <ion-icon name="ellipse" class="ellipse"></ion-icon>
+      <ion-icon name="ellipse" class="ellipse"></ion-icon>
+      <ion-icon name="ellipse" class="ellipse"></ion-icon>
+      <ion-icon name="ellipse" class="ellipse"></ion-icon>
+      <ion-icon name="ellipse" class="ellipse"></ion-icon>
     </>
   );
 };
